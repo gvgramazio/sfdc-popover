43be21cd3d09c31423b35a01a829839265a79f23
Giuseppe Valerio Gramazio <gvgramazio@gmail.com>
2021-03-20 17:10:40 +0100

    Add button-icon and popover

--- force-app/main/default/lwc/iconPopover/iconPopover.js
+++ force-app/main/default/lwc/iconPopover/iconPopover.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class IconPopover extends LightningElement {}
+export default class IconPopover extends LightningElement {
+    @api feedbackState;
+    @api hasBodySmall = false;
+    hasFooter = false;
+    hasHeader = false;
+    @api nubbin;
+    @api width;
+
+    handleFooterSlotChange(event) {
+        this.hasFooter = event.target.assignedElements().length !== 0;
+    }
+
+    handleHeaderSlotChange(event) {
+        this.hasHeader = event.target.assignedElements().length !== 0;
+    }
+
+    togglePopover(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.template.querySelector('c-popover').toggle();
+    }
+}
