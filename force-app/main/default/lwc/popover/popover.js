2df9ee2c434f649198f49b6010944bbfd9ca08b9
Giuseppe Valerio Gramazio <gvgramazio@gmail.com>
2021-03-20 11:24:07 +0100

    Add possibility to open, close and toggle the popover

--- force-app/main/default/lwc/popover/popover.js
+++ force-app/main/default/lwc/popover/popover.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Popover extends LightningElement {}
+export default class Popover extends LightningElement {
+    isOpen = false;
+
+    @api
+    close(event) {
+        this.isOpen = false;
+    }
+
+    @api
+    open(event) {
+        this.isOpen = true;
+    }
+
+    @api
+    toggle(event) {
+        this.isOpen = !this.isOpen;
+    }
+
+    get cssClass() {
+        const baseClasses = ['slds-popover', 'slds-nubbin_left'];
+        baseClasses.push([
+            this.isOpen ? 'slds-visible' : 'slds-hidden'
+        ]);
+        return baseClasses.join(' ');
+    }
+}
