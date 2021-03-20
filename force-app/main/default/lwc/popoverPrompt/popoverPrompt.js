e78b1e23538bbf2c35d8a5057a4e8af723368b5a
Giuseppe Valerio Gramazio <gvgramazio@gmail.com>
2021-03-20 20:19:18 +0100

    Handle position

--- force-app/main/default/lwc/popoverPrompt/popoverPrompt.js
+++ force-app/main/default/lwc/popoverPrompt/popoverPrompt.js
@@ -3,6 +3,7 @@ import { LightningElement, api } from 'lwc';
 export default class PopoverPrompt extends LightningElement {
     hasFooter = false;
     isOpen = false;
+    @api position;
 
     @api
     close(event) {
@@ -23,6 +24,26 @@ export default class PopoverPrompt extends LightningElement {
         const baseClasses = ['slds-popover', 'slds-popover_prompt'];
         if (!this.isOpen) {
             baseClasses.push(['slds-popover_hide']);
+        }
+        switch (this.position) {
+            case 'top':
+                baseClasses.push(['slds-popover_prompt_top']);
+                break;
+            case 'top-left':
+                baseClasses.push(['slds-popover_prompt_top-left']);
+                break;
+            case 'top-right':
+                baseClasses.push(['slds-popover_prompt_top-right']);
+                break;
+            case 'bottom':
+                baseClasses.push(['slds-popover_prompt_bottom-center']);
+                break;
+            case 'bottom-left':
+                baseClasses.push(['slds-popover_prompt_bottom']);
+                break;
+            case 'bottom-right':
+                baseClasses.push(['slds-popover_prompt_bottom-right']);
+                break;
         }
         return baseClasses.join(' ');
     }
