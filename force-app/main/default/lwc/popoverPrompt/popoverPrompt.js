48f9d6c7932458f63d1cb54479dc77f899d1a1fa
Giuseppe Valerio Gramazio <gvgramazio@gmail.com>
2021-03-20 19:23:06 +0100

    Handle close

--- force-app/main/default/lwc/popoverPrompt/popoverPrompt.js
+++ force-app/main/default/lwc/popoverPrompt/popoverPrompt.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class PopoverPrompt extends LightningElement {}
+export default class PopoverPrompt extends LightningElement {
+    isOpen = false;
+
+    @api
+    close(event) {
+        this.isOpen = false;
+    }
+
+    @api
+    open(event) {
+        this.isOpen = true;
+    }
+
+    @api
+    toggle(event) {
+        this.isOpen = !this.isOpen;
+    }
+
+    get cssClass() {
+        const baseClasses = ['slds-popover', 'slds-popover_prompt'];
+        if (!this.isOpen) {
+            baseClasses.push(['slds-popover_hide']);
+        }
+        return baseClasses.join(' ');
+    }
+}
