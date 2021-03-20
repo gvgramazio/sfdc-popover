04a69bacfe980cf5acd3de51b3d51c6587a91424
Giuseppe Valerio Gramazio <gvgramazio@gmail.com>
2021-03-20 12:08:11 +0100

    Add optional header and footer

--- force-app/main/default/lwc/popover/popover.js
+++ force-app/main/default/lwc/popover/popover.js
@@ -2,6 +2,8 @@ import { LightningElement, api } from 'lwc';
 
 export default class Popover extends LightningElement {
     isOpen = false;
+    hasFooter = false;
+    hasHeader = false;
 
     @api
     close(event) {
@@ -20,9 +22,33 @@ export default class Popover extends LightningElement {
 
     get cssClass() {
         const baseClasses = ['slds-popover', 'slds-nubbin_left'];
-        baseClasses.push([
-            this.isOpen ? 'slds-visible' : 'slds-hidden'
-        ]);
+        if (!this.isOpen) {
+            baseClasses.push(['slds-popover_hide']);
+        }
         return baseClasses.join(' ');
+    }
+
+    get cssFooterClass() {
+        const baseClasses = ['slds-popover__footer'];
+        if (!this.hasFooter) {
+            baseClasses.push(['slds-hide']);
+        }
+        return baseClasses.join(' ');
+    }
+
+    get cssHeaderClass() {
+        const baseClasses = ['slds-popover__header'];
+        if (!this.hasHeader) {
+            baseClasses.push(['slds-hide']);
+        }
+        return baseClasses.join(' ');
+    }
+
+    handleFooterSlotChange(event) {
+        this.hasFooter = event.target.assignedElements().length !== 0;
+    }
+
+    handleHeaderSlotChange(event) {
+        this.hasHeader = event.target.assignedElements().length !== 0;
     }
 }
